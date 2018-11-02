79717022317bc637c81b72e525e68b45359bdb00
Nick Santoro <nicholassantoro@Nicholass-MacBook-Pro.local>
2018-11-02 10:49:48 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    eventFraud: [
+      {id: 1, event: "party", fraud: "high"},
+      {id: 1, event: "concert", fraud: "low"},
+      {id: 1, event: "sports", fraud: "medium"}
+    ]  
+  }
+
+  componentDidMount() {
+    console.log("componentdidmount")
+    axios.get('/user?ID=12345')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
       </div>
     );
   }
